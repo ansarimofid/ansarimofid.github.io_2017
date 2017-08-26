29409743bac9dba1c34148f893aced8f6d9ac52d
ansarimofid <ansarimofid@gmail.com>
2017-08-26 18:48:41 +0530

    Feat:Adds Header Section to Home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,10 +3,20 @@
  */
 import React from 'react'
 
+import './Home.css'
+
 const Home = ()=>(
-  <div className="uk-container uk-text-center">
-    <h2>I'm from Home</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque debitis dolor eos esse magnam tempore voluptatibus! Distinctio dolor enim impedit nisi non officiis optio possimus veniam. Accusantium dolor ex fugiat id illum ipsa labore magnam nostrum quod temporibus. Deleniti distinctio dolorum eveniet nesciunt placeat quidem repellat vitae voluptate voluptatibus!</p></div>
+  <div className="home">
+      <div className="hero-wrapper uk-text-center">
+          <div className="uk-text-left hero-container uk-container">
+              <div className="title-deco hero-title color-primary gradient-text">
+                  Hello World
+              </div>
+              <div className="intro uk-text-bold uk-margin-small-left">I'm <span className="color-primary">Mofid Ansari</span></div>
+              <div className="info uk-margin-small-top uk-margin-small-left">I’m <span className="color-primary uk-text-bold">UI Designer</span> and currently 4th year student at IIIT Vadodara</div>
+          </div>
+      </div>
+  </div>
 );
 
 export default Home
