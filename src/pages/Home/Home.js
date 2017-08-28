e3ea34d6a4b8c5e29fae5b3fc8f008a9d954edee
ansarimofid <ansarimofid@gmail.com>
2017-08-28 11:51:24 +0530

    Feat:Adds about & Skill Section

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -10,8 +10,8 @@ const Home = ()=>(
   <div className="home">
       <div className="hero-wrapper uk-text-center">
           <div className="hero-container uk-container">
-              <div className="title-deco hero-title color-primary gradient-text">
-                  Hello World
+              <div className="hero-title color-primary gradient-text">
+                  Hello World!
               </div>
               <div className="intro uk-text-bold uk-margin-small-left">I'm <span className="color-primary">Mofid Ansari</span></div>
               <div className="info uk-margin-small-top uk-margin-small-left">I’m <span className="color-primary uk-text-bold">UI Designer</span> and currently 4th year student at IIIT Vadodara</div>
--- src/pages/Resume/Resume.js
+++ src/pages/Resume/Resume.js
@@ -3,10 +3,58 @@
  */
 import React from 'react'
 
-const Resume = ()=>(
-  <div className="uk-container uk-text-center">
-    <h2>I'm From Resume</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi atque debitis dolor eos esse magnam tempore voluptatibus! Distinctio dolor enim impedit nisi non officiis optio possimus veniam. Accusantium dolor ex fugiat id illum ipsa labore magnam nostrum quod temporibus. Deleniti distinctio dolorum eveniet nesciunt placeat quidem repellat vitae voluptate voluptatibus!</p></div>
+import './resume.css'
+
+const Resume = () => (
+  <div className="uk-container resume uk-text-left uk-margin-large-top">
+    <div is uk-grid>
+      <div className="uk-width-1-6@m"></div>
+      <div className="uk-width-2-3@m">
+        <h2 className="page-title uk-text-bold uk-text-uppercase color-grey-54">Resume</h2>
+
+        <section className="about uk-margin-large-top">
+          <h4 className="title-deco">About</h4>
+          <p>Hi, I’m <span className="color-primary">Mofid Ansari</span>. I’m self-taught <span className="color-primary">UI Designer</span> and student from IIIT Vadodara. While creating websites I
+            really enjoyed the designing part so I started taking it more seriously. From the last 2 years, I’ve been
+            exploring, learning and creating as much as I can to evolve as a better designer.</p>
+        </section>
+
+        <section className="about uk-margin-large-top">
+          <h4 className="title-deco">Skills</h4>
+          <div is uk-grid>
+            <div className="uk-width-1-2@m">
+              <div className="h5 section-subtitle">Core Skills</div>
+              <div className="core-skills-list uk-flex uk-flex-wrap uk-margin-small-top">
+                <div className="core-skill">Web Design</div>
+                <div className="core-skill">Photoshop</div>
+                <div className="core-skill">Sketch</div>
+                <div className="core-skill">Material Design</div>
+                <div className="core-skill">Html & CSS</div>
+              </div>
+            </div>
+            <div className="uk-width-1-2@m">
+              <div className="h5 section-subtitle">Other Skills</div>
+              <div className="other-skills-list  uk-flex uk-flex-wrap uk-margin-small-top">
+                <div className="other-skill">UX</div>
+                <div className="other-skill">Graphic Design</div>
+                <div className="other-skill">Illustration</div>
+                <div className="other-skill">Color Theory</div>
+                <div className="other-skill">Typography</div>
+                <div className="other-skill">Github</div>
+                <div className="other-skill">JavaScript</div>
+                <div className="other-skill">jQuery</div>
+                <div className="other-skill">Ajax</div>
+                <div className="other-skill">NodeJs</div>
+                <div className="other-skill">ReactJ</div>
+                <div className="other-skill">Wordpress</div>
+
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  </div>
 );
 
 export default Resume
