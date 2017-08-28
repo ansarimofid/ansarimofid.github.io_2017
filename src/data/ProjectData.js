dda2b34a3719a60b2065947928dd71bd8a3b9a0e
ansarimofid <ansarimofid@gmail.com>
2017-08-28 10:33:30 +0530

    Adds Project Data

---
+++ src/data/ProjectData.js
@@ -0,0 +1,37 @@
+/**
+ * Created by ansarimofid on 28/08/17.
+ */
+var ProjectData = [];
+export default ProjectData = [
+  {
+    title: 'Minimal Agency Landing Page',
+    tags: ['Web Design', 'UI Design', 'Concept'],
+    thumb: 'img/project_1.jpg',
+    link: 'https://www.behance.net/gallery/51678875/Minimal-Digital-Agency-Landing-Page'
+  },{
+    title: 'WOLO Agency Landing Page',
+    tags: ['Web Design', 'UI Design', 'Concept'],
+    thumb: 'img/project_2.jpg',
+    link: 'https://www.behance.net/gallery/50807543/Digital-Agency-Landing-Page'
+  },{
+    title: 'Music Store Landing Page',
+    tags: ['Web Design', 'UI Design', 'Concept'],
+    thumb: 'img/project_3.jpg',
+    link: 'https://www.behance.net/gallery/50558573/Music-Store-Landing-Page'
+  },{
+    title: 'Cloud Service Landing Page',
+    tags: ['Web Design', 'UI Design', 'Concept'],
+    thumb: 'img/project_4.jpg',
+    link: 'https://www.behance.net/gallery/51452029/Cloud-Service-Landing-Page'
+  },{
+    title: 'Single-Page Portfolio Design',
+    tags: ['Web Design', 'UI Design', 'Concept'],
+    thumb: 'img/project_5.jpg',
+    link: 'https://www.behance.net/gallery/49097867/Portfolio-Design',
+  },{
+    title: 'AnyAudio Android App',
+    tags: ['Web Design', 'UI/UX Design', 'Branding'],
+    thumb: 'img/project_6.jpg',
+    link: 'https://www.behance.net/gallery/49358223/AnyAudio-Play-Stream-Any-Music',
+  }
+]
