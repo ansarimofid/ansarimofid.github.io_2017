18a82414b54f357aa6951b9ed464dfbf700a00ad
ansarimofid <ansarimofid@gmail.com>
2017-08-29 17:07:31 +0530

    Import for images changes

--- src/components/ProjectCards/ProjectCard.js
+++ src/components/ProjectCards/ProjectCard.js
@@ -8,11 +8,12 @@ import './ProjectCard.css'
 class ProjectCard extends Component {
 
   render() {
+    const imgUrl = require('../../'+this.props.cardData.thumb);
     return (
       <div className="uk-width-1-2@s uk-width-1-3@m uk-margin-medium-top project-card">
         <div className="uk-card uk-card-default">
           <a href={this.props.cardData.link} className="uk-card-media-top uk-overflow-hidden"  target="blank">
-            <img src={this.props.cardData.thumb} alt={this.props.cardData.title}/>
+            <img src={imgUrl} alt={this.props.cardData.title}/>
             <div className="img-overlay"></div>
           </a>
           <div className="uk-card-body">
