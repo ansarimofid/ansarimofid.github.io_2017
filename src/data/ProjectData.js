bf6e8ed6f3e39e8e26452a5ff21c959542953c44
ansarimofid <ansarimofid@gmail.com>
2017-08-29 11:45:46 +0530

    Updates project thumbs and adds new projects

--- src/data/ProjectData.js
+++ src/data/ProjectData.js
@@ -4,34 +4,34 @@
 let ProjectData = [];
 export default ProjectData = [
   {
-    title: 'Minimal Agency Landing Page',
-    tags: ['Web Design', 'UI Design', 'Concept'],
-    thumb: 'img/project_1.jpg',
-    link: 'https://www.behance.net/gallery/51678875/Minimal-Digital-Agency-Landing-Page'
+    title: 'Resume Site',
+    tags: ['Web Design', 'UI Design', 'Branding'],
+    thumb: 'img/project_8.jpg',
+    link: 'https://www.behance.net/gallery/54501633/Resume-Site'
+  },{
+    title: 'AnyAudio WebApp',
+    tags: ['Web Design', 'UI Design', 'Branding'],
+    thumb: 'img/project_7.jpg',
+    link: 'https://www.behance.net/gallery/55067021/AnyAudio-WebApp'
   },{
     title: 'WOLO Agency Landing Page',
     tags: ['Web Design', 'UI Design', 'Concept'],
     thumb: 'img/project_2.jpg',
     link: 'https://www.behance.net/gallery/50807543/Digital-Agency-Landing-Page'
   },{
-    title: 'Music Store Landing Page',
-    tags: ['Web Design', 'UI Design', 'Concept'],
-    thumb: 'img/project_3.jpg',
-    link: 'https://www.behance.net/gallery/50558573/Music-Store-Landing-Page'
+    title: 'AnyAudio Android App',
+    tags: ['Web Design', 'UI/UX Design', 'Branding'],
+    thumb: 'img/project_6.jpg',
+    link: 'https://www.behance.net/gallery/49358223/AnyAudio-Play-Stream-Any-Music',
   },{
     title: 'Cloud Service Landing Page',
     tags: ['Web Design', 'UI Design', 'Concept'],
     thumb: 'img/project_4.jpg',
     link: 'https://www.behance.net/gallery/51452029/Cloud-Service-Landing-Page'
   },{
-    title: 'Single-Page Portfolio Design',
+    title: 'Music Store Landing Page',
     tags: ['Web Design', 'UI Design', 'Concept'],
-    thumb: 'img/project_5.jpg',
-    link: 'https://www.behance.net/gallery/49097867/Portfolio-Design',
-  },{
-    title: 'AnyAudio Android App',
-    tags: ['Web Design', 'UI/UX Design', 'Branding'],
-    thumb: 'img/project_6.jpg',
-    link: 'https://www.behance.net/gallery/49358223/AnyAudio-Play-Stream-Any-Music',
+    thumb: 'img/project_3.jpg',
+    link: 'https://www.behance.net/gallery/50558573/Music-Store-Landing-Page'
   }
 ]
